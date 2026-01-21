b1d7516e16a4768d0ba83082c22f80ae2e065924
propertyeeleslie-web <property.eeleslie@gmail.com>
2026-01-22 00:03:47 +0800

    Update properties.js

--- api/properties.js
+++ api/properties.js
@@ -1,3 +1,10 @@
+export default async function handler(req,res){
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers","Content-Type,Authorization");
+
+  if(req.method==="OPTIONS") return res.status(200).end();
+
 import { createClient } from "@supabase/supabase-js";
 import * as jose from "jose";
 
